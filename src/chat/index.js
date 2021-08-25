ec7153c9e7af927be14686c95f67dcd9a37f0a6f
18871002288 <18871002288@163.com>
2021-08-25 17:31:09 +0800

    Change ChatFeed to hook

--- src/chat/index.js
+++ src/chat/index.js
@@ -1,31 +1,24 @@
 'use strict'
-
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types';
 import ChatBubble from './ChatBubble';
 import ChatInput from './ChatInput';
 import "./style.scss";
 
-export default class ChatFeed extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      messages: props.messages || [],
-    }
-  }
+export default function ChatFeed(props) {
 
-  _scrollToBottom = () => {
-    const { chat } = this.refs;
-    if(chat !== undefined){
-      const scrollHeight = chat.scrollHeight;
-      const height = chat.clientHeight;
+  const chat = useRef(null)
+
+  const scrollToBottom = () => {
+    if (chat !== undefined) {
+      const scrollHeight = chat.current.scrollHeight;
+      const height = chat.current.clientHeight;
       const maxScrollTop = scrollHeight - height;
-      ReactDOM.findDOMNode(chat).scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
+      chat.current.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
     }
   }
 
-  _renderGroup = (messages, index, id) => {
+  const renderGroup = (messages, index, id) => {
     let group = []
 
     for (let i = index; messages[i] ? messages[i].id == id : false; i--) {
@@ -47,41 +40,40 @@ export default class ChatFeed extends Component {
     )
   }
 
-  _renderMessages = (messages) => {
+  const renderMessages = (messages) => {
     var message_nodes = messages.map((curr, index) => {
       if (
         (messages[index + 1] ? false : true) ||
         messages[index + 1].id != curr.id
       ) {
-        return this._renderGroup(messages, index, curr.id)
+        return renderGroup(messages, index, curr.id)
       }
     })
     return message_nodes
   }
 
-  render() {
+  useEffect(() => {
     window.setTimeout(() => {
-      this._scrollToBottom()
+      scrollToBottom()
     }, 10)
+  })
 
-    return (
-      <div id="chat-panel" className='chat-panel'>
+  return (
+    <div id="chat-panel" className='chat-panel'>
 
-        <div className='title-panel'>
-          <span className='title-chat'>Chat</span>
-        </div>
-
-        <div ref="chat" className='chat-history'>
-          <div>
-            {this._renderMessages(this.props.messages)}
-          </div>
-        </div>
-        <ChatInput onSendMessage={this.props.onSendMessage}/>
+      <div className='title-panel'>
+        <span className='title-chat'>Chat</span>
       </div>
-    )
-  }
-}
 
+      <div ref={chat} className='chat-history'>
+        <div>
+          {renderMessages(props.messages)}
+        </div>
+      </div>
+      <ChatInput onSendMessage={props.onSendMessage} />
+    </div>
+  )
+}
 
 ChatFeed.propTypes = {
   isTyping: PropTypes.bool,
